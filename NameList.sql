cf8607b3717f5c1932401bcdcd889aae1678dd30
ryo_starlight <42905813+doorbrl@users.noreply.github.com>
2020-04-28 12:15:55 +0900

    Update NameList.sql

--- NameList.sql
+++ NameList.sql
@@ -1,25 +1,26 @@
 create  table StarLight(
+KEY_NO int NOT NULL,
 name varchar(255),
 birthday varchar(255),
-Soleil int(2)
-Luminous int(2)
+Soleil int(1)
+Luminous int(1)
 Brand　varchar(255),
 );
 
 -- 複数レコード一括INSERT
 INSERT INTO StarLight
-  (name, birthday, Soleil, Luminous, Brand)
+  (KEY_NO, name, birthday, Soleil, Luminous, Brand)
 VALUES 
-  ('星宮いちご', '3月15日', 1, 0, 'AngerySugar'),
-  ('霧矢あおい', '1月31日', 1, 0, 'FuturingGIRL'),
-  ('紫吹蘭', '8月3日', 1, 0, 'SpicyAGEHA'),
-  ('大空あかり', '4月1日', 0, 1, 'DreamyCROWN'),
-  ('氷上スミレ', '', 0, 1, 'LoliGothic'),
-  ('新条ひなき', '', 0, 1, 'VividKiss');
+  (1, '星宮いちご', '3月15日', 1, 0, 'AngerySugar'),
+  (2, '霧矢あおい', '1月31日', 1, 0, 'FuturingGIRL'),
+  (3, '紫吹蘭', '8月3日', 1, 0, 'SpicyAGEHA'),
+  (4, '大空あかり', '4月1日', 0, 1, 'DreamyCROWN'),
+  (5, '氷上スミレ', '', 0, 1, 'LoliGothic'),
+  (6, '新条ひなき', '', 0, 1, 'VividKiss');
 
   
-  
 create  table DreamAcademy(
+KEY_NO int NOT NULL,
 name varchar(255),
 birthday varchar(255),
 Brand　varchar(255),
@@ -27,9 +28,12 @@ Brand　varchar(255),
 
 -- 複数レコード一括INSERT
 INSERT INTO DreamAcademy
-  (name, birthday, Brand)
+  (KEY_NO, name, birthday, Brand)
 VALUES 
-  ('音城セイラ', '3月15日', 'AngerySugar'),
-  ('冴草きい', '1月31日', 'FuturingGIRL'),
-  ('風沢そら', '8月3日', 'SpicyAGEHA'),
-  ('姫里マリア', '4月1日', 'DreamyCROWN');
+  (1, '音城セイラ', '3月15日', 'AngerySugar'),
+  (2, '冴草きい', '1月31日', 'FuturingGIRL'),
+  (3, '風沢そら', '8月3日', 'SpicyAGEHA'),
+  (4, '姫里マリア', '4月1日', 'DreamyCROWN');
+
+-- 外部結合 FULL OUTER JOIN
+SELECT * FROM StarLight FULL OUTER JOIN DreamAcademy ON StarLight.id = DreamAcademy.id;
