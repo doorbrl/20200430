02d0e042b25f1c27aef90c86ae271f2353e0a800
ryo_starlight <42905813+doorbrl@users.noreply.github.com>
2020-04-29 21:08:52 +0900

    Update NameList.sql

--- NameList.sql
+++ NameList.sql
@@ -1,23 +1,32 @@
-create  table StarLight(
+CREATE TABLE StarLight(
 KEY_NO int NOT NULL PRIMARY KEY,
 name varchar(255),
 birthday varchar(255),
-Soleil int(1)
-Luminous int(1)
-Brand　varchar(255),
+Soleil varchar(2),
+Luminous varchar(2),
+Brand　varchar(255)
 );
 
 -- 複数レコード一括INSERT
 INSERT INTO StarLight
   (KEY_NO, name, birthday, Soleil, Luminas, Brand)
 VALUES 
-  (1, '星宮いちご', '3月15日', 1, 0, 'Angely Sugar'),
-  (2, '霧矢あおい', '1月31日', 1, 0, 'FUTURING GIRL'),
-  (3, '紫吹蘭', '8月3日', 1, 0, 'SPICY AGEHA'),
-  (4, '大空あかり', '4月1日', 0, 1, 'Dreamy Crown'),
-  (5, '氷上スミレ', '10月20日', 0, 1, 'LoLi GoThiC'),
-  (6, '新条ひなき', '6月11日', 0, 1, 'Vivid Kiss');
-
+  (1, '星宮いちご', '3月15日', '1', '0', 'Angely Sugar'),
+  (2, '霧矢あおい', '1月31日', '1', '0', 'FUTURING GIRL'),
+  (3, '紫吹蘭', '8月3日', '1', '0', 'SPICY AGEHA'),
+  (4, '大空あかり', '4月1日', '0', '1', 'Dreamy Crown'),
+  (5, '氷上スミレ', '10月20日','0', '1', 'LoLi GoThiC'),
+  (6, '新条ひなき', '6月11日', '0', '1', 'Vivid Kiss');
+  
+mysql> CREATE TABLE StarLight (
+    -> KEY_NO int NOT NULL PRIMARY KEY,
+    -> name varchar(255),
+    -> birthday varchar(255),
+    -> Soleil varchar(2),
+    -> Luminas varchar(2),
+    -> Brand varchar(50)
+    -> )
+    -> ;
   
 create  table DreamAcademy(
 KEY_NO int NOT NULL PRIMARY KEY,
