e6f5fad9b3473c791e7db545fe949a9f3f49643e
doorbel <ryohakama.kuc@gmail.com>
2020-04-30 08:44:11 +0900

    Git-Eclipseの連携テスト

--- NameList.sql
+++ NameList.sql
@@ -10,24 +10,15 @@ Brand varchar(50)
 -- 複数レコード一括INSERT
 INSERT INTO StarLight
   (KEY_NO, name, birthday, Soleil, Luminas, Brand)
-VALUES 
+VALUES
   (1, '星宮いちご', '3月15日', '1', '0', 'Angely Sugar'),
   (2, '霧矢あおい', '1月31日', '1', '0', 'FUTURING GIRL'),
   (3, '紫吹蘭', '8月3日', '1', '0', 'SPICY AGEHA'),
   (4, '大空あかり', '4月1日', '0', '1', 'Dreamy Crown'),
   (5, '氷上スミレ', '10月20日','0', '1', 'LoLi GoThiC'),
   (6, '新条ひなき', '6月11日', '0', '1', 'Vivid Kiss');
-  
-mysql> CREATE TABLE StarLight (
-    -> KEY_NO int NOT NULL PRIMARY KEY,
-    -> name varchar(255),
-    -> birthday varchar(255),
-    -> Soleil varchar(2),
-    -> Luminas varchar(2),
-    -> Brand varchar(50)
-    -> )
-    -> ;
-  
+
+
 create  table DreamAcademy(
 KEY_NO int NOT NULL PRIMARY KEY,
 name varchar(255),
@@ -38,7 +29,7 @@ Brand varchar(50)
 -- 複数レコード一括INSERT
 INSERT INTO DreamAcademy
   (KEY_NO, name, birthday, Brand)
-VALUES 
+VALUES
   (1, '音城セイラ', '11月11日', 'Swing ROCK'),
   (2, '冴草きい', '12月3日', 'MAGICAL TOY'),
   (3, '風沢そら', '	10月2日', 'BOHEMIAN SKY'),
