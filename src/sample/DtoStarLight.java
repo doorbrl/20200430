8cebd3949558c9118700fc3cbe9fb4f79b4870ca
doorbel <ryohakama.kuc@gmail.com>
2020-04-30 09:18:29 +0900

    SQLから接続する処理を追加

---
+++ src/sample/DtoStarLight.java
@@ -0,0 +1,24 @@
+package sample;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Setter
+@Getter
+public class DtoStarLight {
+
+	private int id;
+
+	private String name;
+
+	private int birthday;
+
+	private String Soleil;
+
+	private String Luminas;
+
+	private String Brand;
+
+}
---
+++ src/sample/sqlConnect.java
@@ -0,0 +1,62 @@
+package sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class sqlConnect {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		try {
+			Class.forName(PropertyUtil.getProperty("className"));
+
+			//MySQLに接続するための情報をプロパティファイルから取得
+			String driver = PropertyUtil.getProperty("driver");
+			String user = PropertyUtil.getProperty("user");
+			String password = PropertyUtil.getProperty("password");
+
+			//取得した情報をもとにMySQLへ接続処理
+			Connection conn = DriverManager.getConnection(driver,user,password);
+
+			Statement st = conn.createStatement();
+			if(st != null) {
+				System.out.println("接続完了");
+			}
+
+			//SELECT文を取得、実行
+			String select = PropertyUtil.getProperty("select_starlight");
+			PreparedStatement ps = conn.prepareStatement(select);
+
+			System.out.println("---------------START-----------------");
+			try(ResultSet rs = ps.executeQuery()){
+				while (rs.next()) {
+					System.out.println(
+							"No." +rs.getInt("KEY_NO") + " " +
+									"名前：" +rs.getString("name") + " " +
+									"誕生日："+rs.getString("birthday")+ " "+
+									"好きなブランド："+rs.getString("Brand"));
+					if(rs.getString("Soleil").equals("1")) {
+						System.out.println("ソレイユ：〇");
+					}else {
+						System.out.println("ソレイユ：-");
+					}
+					if(rs.getString("Luminas").equals("1")) {
+						System.out.println("ルミナス;〇");
+					}else {
+						System.out.println("ルミナス：-");
+					}
+				}
+			}
+			System.out.println("---------------END-----------------");
+		} catch (ClassNotFoundException e) {
+			System.out.println("ドライバを読み込めませんでした "+ e);
+		} catch (SQLException e) {
+			System.out.println("データベース接続エラー"+ e);
+
+		}
+	}
+}
