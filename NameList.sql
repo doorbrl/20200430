63913df0111986407b04880aaa11a228ee02ad99
ryo_starlight <42905813+doorbrl@users.noreply.github.com>
2020-08-01 19:35:47 +0900

    Update NameList.sql
    以下の変更を加えた
    
    ・StarilightテーブルのCreate文について、TristarとStarAnisの項目を追加
    ・StarilightテーブルのINSERT文について、STAR☆ANISの残りメンバーとあかジェネの主要メンバーを追加
    ・StarilightテーブルのCreate文とINSERT文に、世代を示す項目geneをKEY_IDに代わって投入
    (I：いちご世代、A：あかり世代)

--- NameList.sql
+++ NameList.sql
@@ -1,24 +1,33 @@
 CREATE TABLE StarLight(
-KEY_NO int NOT NULL PRIMARY KEY,
+gene varchar(1) NOT NULL PRIMARY KEY,
 name varchar(255),
 birthday varchar(255),
 Soleil int(1),
 Luminous int(1),
+Tristar int(1),
+StarAnis int(1),
 Brand varchar(50)
 );
 
 -- 複数レコード一括INSERT
 INSERT INTO StarLight
-  (KEY_NO, name, birthday, Soleil, Luminas, Brand)
+  (gene, name, birthday, Soleil, Luminas, Tristar, StarAnis, Brand)
 VALUES
-  (1, '星宮いちご', '3月15日', '1', '0', 'Angely Sugar'),
-  (2, '霧矢あおい', '1月31日', '1', '0', 'FUTURING GIRL'),
-  (3, '紫吹蘭', '8月3日', '1', '0', 'SPICY AGEHA'),
-  (4, '大空あかり', '4月1日', '0', '1', 'Dreamy Crown'),
-  (5, '氷上スミレ', '10月20日','0', '1', 'LoLi GoThiC'),
-  (6, '新条ひなき', '6月11日', '0', '1', 'Vivid Kiss');
-
-
+  ('I', '星宮いちご', '3月15日', 1, 0, 0, 1, 'Angely Sugar'),
+  ('I', '霧矢あおい', '1月31日', 1, 0, 0, 1, 'FUTURING GIRL'),
+  ('I', '紫吹蘭', '8月3日', 1, 0, 0, 1, 'SPICY AGEHA'),
+  ('I', '有栖川おとめ', '5月5日', 0, 0, 0, 1, 'HAPPY RAINBOW'),
+  ('I', '北大路さくら', '4月6日', 0, 0, 0, 1, 'Aurora Fantasy'),
+  ('I', '一ノ瀬かえで', '11月23日', 0, 0, 1, 1, 'MAGICAL TOY'),
+  ('I', '藤堂ユリカ', '12月26日', 0, 0, 1, 1, 'LoLi GoThiC'),
+  ('I', '神崎美月', '9月18日', 0, 0, 1, 1, 'LOVE MOONRISE'),
+  ('A', '大空あかり', '4月1日', 0, 1, 0, 0, 'Dreamy Crown'),
+  ('A', '氷上スミレ', '10月20日',0, 1, 0, 0, 'LoLi GoThiC'),
+  ('A', '新条ひなき', '6月11日', 0, 1, 0, 0, 'Vivid Kiss'),
+  ('A', '紅林珠璃', '7月31日', 0, 0, 0, 0, 'Sangria Rosa'),
+  ('A', '黒沢凛', '1月1日', 0, 0, 0, 0, 'Dance Fusion'),
+  ('A', '天羽まどか', '2月14日', 0, 0, 0, 0, 'Angely Sugar');
+  
 create  table DreamAcademy(
 KEY_NO int NOT NULL PRIMARY KEY,
 name varchar(255),
